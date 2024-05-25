35960c4efbf4e785f68cf1c4c2b090e96e3be1ae
sean842 <seanbleier123@gmail.com>
2024-05-25 17:26:32 +0300

    V6

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using DemocracyGame.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,32 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+       new[] { "application/octet-stream" });
+});
+
 var app = builder.Build();
+
+app.UseResponseCompression();
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddDefaultPolicy(
+//        builder =>
+//        {
+//            builder.WithOrigins("https://example.com")
+//                .AllowAnyHeader()
+//                .WithMethods("GET", "POST")
+//                .AllowCredentials();
+//        });
+//});
+
+//var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,7 +52,10 @@ app.UseStaticFiles();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+// UseCors must be called before MapHub.
+app.UseCors();
 
 app.MapControllers();
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
---
+++ wwwroot/js/Chat.js
@@ -0,0 +1,40 @@
+﻿
+const baseUrl = "https://localhost:7288/";
+const hubUrl = baseUrl + "ChatHub";
+const chathub = new signalR.HubConnectionBuilder()
+    .withUrl(hubUrl)
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    getHub();
+});
+
+function getHub() {
+    chathub.start()
+        .then(() => {
+            subscribeToHubEvents();
+        })
+        .catch(error => {
+            console.error(error.toString());
+        });
+}
+
+function subscribeToHubEvents() {
+    chathub.on("ReceiveMessage", (user, message) => {
+        const li = document.createElement("li");
+        li.innerText = `${user}: ${message} `;
+        document.getElementById("messageList").appendChild(li);
+    });
+}
+
+
+function clickBTN() {
+    const username = document.getElementById("userInput").value;
+    const message = document.getElementById("messageInput").value;
+
+    chathub.invoke("SendMessage", username, message);
+
+}
+
